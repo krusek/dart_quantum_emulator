a1dc917c67d30cc5e340cf7a8983f5e3de3c32ec
Korben Rusek <korben.rusek@gmail.com>
2019-07-20 07:44:19 -0700

    vector class
    Not 100% cure if / how I'll use this. Probably will be used to perform measurements.

---
+++ lib/linear_algebra/vector.dart
@@ -0,0 +1,71 @@
+
+import 'dart:math';
+
+import 'package:complex/complex.dart';
+
+class Vector {
+  List<Complex> elements;
+  Vector(this.elements);
+  Vector.zero(int length): this([]);
+  Vector.double(List<double> doubles):
+    this(doubles.map((d) {return Complex(d);}).toList());
+  
+  double norm() {
+    final c = this.elements
+      .map((c){return c*c.conjugate();})
+      .reduce((c,d){return c+d;}).abs();
+    return pow(c, 0.5);
+  }
+
+  Vector normalized() {
+    final n = norm();
+    assert(n > 0);
+
+    return this * (1.0 / norm());
+  }
+
+  String toString() {
+    final strings = elements.map((c) {return c.toString();});
+    final joined = strings.join(", ");
+    return "[$joined]";
+  }
+
+  operator  +(Vector v) {
+    assert(v.elements.length == this.elements.length, "vectors must be the same size for operations, ${this.elements.length} vs ${v.elements.length}");
+    
+    final result = zip(v.elements, this.elements).map((t) {
+      return t.one + t.zero;
+    }).toList();
+    return Vector(result);
+  }
+  operator  -(Vector v) {
+    assert(v.elements.length == this.elements.length, "vectors must be the same size for operations, ${this.elements.length} vs ${v.elements.length}");
+    
+    final result = zip(v.elements, this.elements).map((t) {
+      return t.one - t.zero;
+    }).toList();
+    return Vector(result);
+  }
+
+  operator *(double d) {
+    final result = this.elements.map((t) {
+      return t*d;
+    }).toList();
+    return Vector(result);
+  }
+}
+
+class Tuple<T,U> {
+  T zero;
+  U one;
+
+  Tuple({this.zero, this.one});
+}
+
+Iterable<Tuple<T,U>> zip<T, U>(Iterable<T> left, Iterable<U> right) sync* {
+  final lefti = left.iterator;
+  final righti = right.iterator;
+  while (lefti.moveNext() && righti.moveNext()) {
+    yield Tuple<T,U>(zero: lefti.current, one: righti.current);
+  }
+}
---
+++ test/linear_algebra/vector_test.dart
@@ -0,0 +1,53 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+
+import 'dart:math';
+
+import 'package:complex/complex.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quantum_emulator/linear_algebra/vector.dart';
+
+void main() {
+  group('vector operations', () {
+    test('Test vector equivalent of H gate followed by H gate', () {
+      final v = Vector.double([1, 1]).normalized();
+      final c1 = (v.elements[0] + v.elements[1]) / pow(2, 0.5);
+      final c2 = (v.elements[0] - v.elements[1]) / pow(2, 0.5);
+      // renormalizing replaces 0.99999999 with 1
+      final v2 = Vector([c1, c2]).normalized();
+      final elements = v2.elements;
+      assert(elements[0].real == 1.0);
+      assert(elements[1] == Complex.ZERO);
+    });
+
+    test('Test vector addition', () {
+      final v = Vector.double([2,4]);
+      final v2 = Vector.double([3,6]);
+      final a = v + v2;
+      assert(a.elements[0].real == 5);
+      assert(a.elements[1].real == 10);
+    });
+
+    test('Test vector subtraction', () {
+      final v = Vector.double([2,4]);
+      final v2 = Vector.double([3,6]);
+      final a = v - v2;
+      expect(a.elements[0].real, -1);
+      expect(a.elements[1].real, -2);
+    });
+
+    test('test vector and double multiplication', () {
+      final v = Vector.double([2,4]);
+      final a = v * 0.5;
+      expect(a.elements[0].real, 1);
+      expect(a.elements[1].real, 2);
+    });
+  });
+
+
+}
